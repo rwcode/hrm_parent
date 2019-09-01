bde7f48ee6677dc965451a010ccc041a3bfc4521
rwcode <754638716@qq.com>
2019-09-01 12:09:46 +0800

    提交

---
+++ hrm_sysmanage_parent/hrm_sysmanage_service/src/main/java/com/ranwei/hrm/service/impl/SystemdictionaryitemServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ranwei.hrm.service.impl;
+
+import com.ranwei.hrm.domain.Systemdictionaryitem;
+import com.ranwei.hrm.mapper.SystemdictionaryitemMapper;
+import com.ranwei.hrm.service.ISystemdictionaryitemService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author ranwei
+ * @since 2019-08-30
+ */
+@Service
+public class SystemdictionaryitemServiceImpl extends ServiceImpl<SystemdictionaryitemMapper, Systemdictionaryitem> implements ISystemdictionaryitemService {
+
+}
